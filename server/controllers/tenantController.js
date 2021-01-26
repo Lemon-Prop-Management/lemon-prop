ec85fa5b0702370c38151b0635523ffb951a3cc7
Andrew Smith <topcodeandrew@gmail.com>
2021-01-26 11:20:44 -0700

    editUser controller done

--- server/controllers/tenantController.js
+++ server/controllers/tenantController.js
@@ -1,24 +1,33 @@
 module.exports = ({
- editUser: async (req, res) => {
-  const db = await req.app.get('db')
+    editUser: async (req, res) => {
+        const db = await req.app.post('db')
+        const { user_id } = req.params
+        const { first_name, last_name, phone, email } = req.body
 
- },
- addMr: async (req, res) => {
-  const db = await req.app.get('db')
+        await db.mgr.mgr_edit_one_tenant([user_id, first_name, last_name, phone, email])
+            .then(updatedTenant => {
+                res.status(200).send(updatedTenant)
+            })
+            .catch(err => console.log(err))
+    },
 
- },
- getAllMr: async (req, res) => {
-  const db = await req.app.get('db')
 
- },
- getOneMr: async (req, res) => {
-  const db = await req.app.get('db')
+    addMr: async (req, res) => {
+        const db = await req.app.get('db')
 
- },
- getAllPayments: async (req, res) => {
-  const db = await req.app.get('db')
+    },
+    getAllMr: async (req, res) => {
+        const db = await req.app.get('db')
 
- }
+    },
+    getOneMr: async (req, res) => {
+        const db = await req.app.get('db')
+
+    },
+    getAllPayments: async (req, res) => {
+        const db = await req.app.get('db')
+
+    }
 })
 
 
