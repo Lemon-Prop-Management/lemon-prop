364e70834c3bef6269c8ce488cfecd641547aef2
Brooke Miller <brookecm28@gmail.com>
2021-02-01 16:55:44 -0700

    Tenant tab view built, edit button half-functional

--- src/Components/Landing/Login.js
+++ src/Components/Landing/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { loginUser } from '../../redux/reducer'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Login = props => {
   const [email, setEmail] = useState('')
