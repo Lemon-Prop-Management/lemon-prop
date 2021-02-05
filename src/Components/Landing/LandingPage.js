b427bd95f52b82bf3547b3107a111ce8eaa48d31
Jake Bryant <jaicb23@gmail.com>
2021-02-04 23:25:56 -0700

    landingpage

--- src/Components/Landing/LandingPage.js
+++ src/Components/Landing/LandingPage.js
@@ -2,18 +2,35 @@ import React from 'react'
 import Login from './Login'
 import '../../scss/_landing.scss'
 import logo from '../../img/lemon_prop.png'
-/*
-https://stackoverflow.com/questions/40982337/rotated-squares-as-an-image
-*/
+import rental1 from '../../img/rental1.png'
+import rental2 from '../../img/rental2.png'
+import rental7 from '../../img/rental7.png'
 
 const LandingPage = props => {
   return (
     <div className="landing">
       <div className="header">
         <img className="logo" src={logo} ></img>
-        <h1 className="landing-page-adjust">Lemon Prop Management</h1>
+        <h1 className="landing-page-adjust">LemonProp Management</h1>
         {/* The Login that is rendered below will eventually be moved to an onClick function for a popover. We are displaying it now to test further functionality while we are still getting the popover up and running */}
         <Login {...props} />
+      </div>
+      <div className="landing-middle">
+        <h2>We make finding a new home</h2>
+        <h1>EASY!</h1>
+        <hr />
+        <h3>This could be your next rental!</h3>
+      </div>
+      <div className="rental-row">
+        <div className="rental-container">
+          <img src={rental1} ></img>
+        </div>
+        <div className="rental-container">
+          <img src={rental2} ></img>
+        </div>
+        <div className="rental-container">
+          <img src={rental7} ></img>
+        </div>
       </div>
       <div className="grid">
         <div id="diamond1" className="diamond-med"></div>
