ad404f8b68c727b8cf0814cf9f3c436a05bbdc33
Jacob Bryant <73540703+JaicB@users.noreply.github.com>
2023-11-09 16:15:15 -0700

    roll back

--- db/auth/auth_add_one_user.sql
+++ db/auth/auth_add_one_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users
 (first_name, last_name, phone, email, hash, due_date, pet, approved, admin, prop_id, reset_password_token, reset_password_expires)
 VALUES
-($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
 returning *;
